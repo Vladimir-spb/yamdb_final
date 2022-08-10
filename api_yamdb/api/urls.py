fd7d184577fbb67c22be0428605dcaaf4c7803a4
unknown <reos007@mail.ru>
2022-08-10 21:11:32 +0300

    22

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -37,7 +37,7 @@ router_v1.register(r'genres', views.GenreViewSet, basename='genres')
 app_name = 'api'
 urlpatterns = [
     path(
-        f'{API_VERSION}/redoc/',
+        'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc',
     ),
